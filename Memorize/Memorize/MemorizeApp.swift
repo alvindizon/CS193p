c6bd3449a74a5d417de63a2caa8ab443b63a39e3
Alvin Dizon <alvin.dizon91@gmail.com>
2024-03-27 16:21:08 +0800

    Lecture 1
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alvin Dizon on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
