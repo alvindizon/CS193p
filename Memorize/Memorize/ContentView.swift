1e0cb81b04e555d605775fc221349dbcf888e744
Alvin Dizon <alvin.dizon91@gmail.com>
2024-04-30 22:55:58 +0800

    Assignment 1
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,63 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = [ "👻", "🎃", "🕷️","😈", "😈", "👻", "🎃", "🕷️","😈", "🏩", "🧨", "🍭"]
-    @State var cardCount = 4
+    let emojis: [String] = [ "👻", "🎃", "🕷️","😈", "👿", "👻", "🎃", "🕷️","👁️", "🏩", "🧨", "🍭"]
+    let theme2: [String] = [ "🚗", "🚃", "🚄","🚈", "🛟", "✈️", "🛩️", "🚢","🚀", "🚊", "🚒", "🛴"]
+    let theme3: [String] = [ "😇", "😘", "🥳","🤪", "😎", "🤩", "😏", "😌","😙", "🤪", "😋", "☺️"]
+    let themeIcons: [String] = ["house", "car", "face.smiling"]
+    let themeTitles: [String] = ["Default", "Transport", "Face"]
+
+    @State var selectedThemeIndex = 0
+
     var body: some View {
         VStack {
+            Text("Memorize!").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             ScrollView {
                 cards
             }
             Spacer()
-            cardCountAdjusters
+            buttons
         }
         .padding()
     }
+    func getAllThemes() -> [[String]] {
+        return [emojis, theme2, theme3]
+    }
+
+    func getTheme(selectedIndex: Int) -> [String] {
+        return getAllThemes()[selectedIndex]
+    }
+
+    var themeChoosers: some View {
+        HStack {
+            ForEach(getAllThemes().indices, id: \.self) { index in
+                Button(action: { 
+                    selectedThemeIndex = index
+                }, label: {
+                    VStack {
+                        Image(systemName: themeIcons[index])
+                        Text(themeTitles[index]).font(.caption)
+                    }
+                })
+
+            }
+        }
+    }
+
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
+        LazyVGrid(columns: [    GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                                        GridItem(.flexible())]) {
+            ForEach(0..<emojis.count, id: \.self) { index in
+                CardView(content: getTheme(selectedIndex: selectedThemeIndex).shuffled()[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
     }
-    var cardCountAdjusters: some View {
+    var buttons: some View {
         HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-
+            themeChoosers
         }
         .imageScale(.large)
         .font(.largeTitle)
-    }
-
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-    }
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-
-    var cardAdder: some View {
-        Button(action: {
-            if cardCount < emojis.count {
-                cardCount += 1
-            }
-        }, label: {
-            Image(systemName: "rectangle.stack.badge.plus.fill")
-        })
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = true
+    @State var isFaceUp = false
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
